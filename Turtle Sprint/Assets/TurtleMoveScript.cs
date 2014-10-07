eb5dcd49640d36ac025b1d196a413444e521d96e
Ethan Goldman-Kirst <ethan@d-69-91-134-145.dhcp4.washington.edu>
2014-10-07 12:56:34 -0700

    added turtle move script

---
+++ Turtle Sprint/Assets/TurtleMoveScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurtleMoveScript : MonoBehaviour {
+
+	public float maxSpeed = 5f;		
+	
+	void Start() {
+		
+	}
+	
+	void FixedUpdate () {
+		// Cache the horizontal input.
+		float move = Input.GetAxis("Horizontal");
+		
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+	}
+}
