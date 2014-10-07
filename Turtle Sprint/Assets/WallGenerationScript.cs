2b87dab208590261eff0e5a9e330a823f10b684c
Ethan Goldman-Kirst <ethan@d-69-91-134-145.dhcp4.washington.edu>
2014-10-07 13:40:07 -0700

    made x axis of generated walls random

--- Turtle Sprint/Assets/WallGenerationScript.cs
+++ Turtle Sprint/Assets/WallGenerationScript.cs
@@ -1,18 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class WallGenerationScript : MonoBehaviour {
 
 	public float spawnTime = 5f;		// The amount of time between each spawn.
 	public float spawnDelay = 3f;		// The amount of time before spawning starts.
 	public Transform enemyPrefab;
+
+	private int gap;
+	private int leftX;
+	private System.Random rnd;
 	
 	void Start () {
+		rnd = new System.Random ();
+		gap = 14;
+		leftX = -7;
+
 		// Start calling the Spawn function repeatedly after a delay .
 		InvokeRepeating("Spawn", spawnDelay, spawnTime);
 	}
 	
 	void Spawn () {
-		Instantiate(enemyPrefab, new Vector2(0, 4), transform.rotation);
+		Instantiate(enemyPrefab, new Vector2(leftX, 7), transform.rotation);
+		Instantiate(enemyPrefab, new Vector2(leftX + gap, 7), transform.rotation);
+
+		leftX = rnd.Next (leftX - 1, leftX + 2);
 	}
 }
