e78148664d62559720cc8dc86e99fc5178357c32
Ethan Goldman-Kirst <ethan@d-69-91-134-145.dhcp4.washington.edu>
2014-10-07 13:13:32 -0700

    added wall piece object

---
+++ Turtle Sprint/Assets/WallMoveScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallMoveScript : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(5, 5);
+	public Vector2 direction = new Vector2(0, -1);
+	private Vector2 movement;
+	
+	void Update() {
+		movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
+	}
+	
+	void FixedUpdate() {
+		rigidbody2D.velocity = movement;
+	}
+}
