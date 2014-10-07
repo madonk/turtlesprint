b77c92bdc24d9869b0c508cd473911c0912c2915
Ethan Goldman-Kirst <ethan@d-69-91-134-145.dhcp4.washington.edu>
2014-10-07 13:16:37 -0700

    added wall generation script

---
+++ Turtle Sprint/Assets/WallGenerationScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallGenerationScript : MonoBehaviour {
+
+	public float spawnTime = 5f;		// The amount of time between each spawn.
+	public float spawnDelay = 3f;		// The amount of time before spawning starts.
+	public Transform enemyPrefab;
+	
+	void Start () {
+		// Start calling the Spawn function repeatedly after a delay .
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+	}
+	
+	void Spawn () {
+		Instantiate(enemyPrefab, new Vector2(0, 4), transform.rotation);
+	}
+}
